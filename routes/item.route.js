39067b3b6c2f3ba8685858b9e956573f5faca975
Abdulrahman-S <abdulrahmansaddiq@gmail.com>
2020-03-31 17:33:36 +0300

    change homeEJS and itemRoute

--- routes/item.route.js
+++ routes/item.route.js
@@ -1,26 +1,31 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
-const Item=require("../models/item.model");
+const Item = require("../models/item.model");
 const flash = require('connect-flash')
 const { check, validationResult } = require("express-validator");
 const multer = require("multer");
 const path = require("path");
 
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, "public/uploads");
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads");
     },
-    filename: function(req, file, cb) {
-      let fileExtension = path.extname(file.originalname).split(".")[1];
-      cb(null, file.fieldname + "-" + Date.now() + "." + fileExtension);
+    filename: function (req, file, cb) {
+        let fileExtension = path.extname(file.originalname).split(".")[1];
+        cb(null, file.fieldname + "-" + Date.now() + "." + fileExtension);
     }
-  });
-  var upload = multer({ storage: storage });
+});
+var upload = multer({ storage: storage });
 
 router.get("/home", (request, response) => {
     // request.user
     User.find().then(users => {
-        response.render("home", { users });
+        Item.find().then(item => {
+            response.render("home", { item, users });
+        })
+            .catch(err => {
+                request.flash("error", err)
+            })
     });
 });
 
@@ -28,41 +33,41 @@ router.get("/item", (request, response) => {
     Item.find().then(item => {
         response.render("item", { item });
     })
-    .catch(err =>{
-        request.flash("error",err)
-    })
-   });
+        .catch(err => {
+            request.flash("error", err)
+        })
+});
 
 router.get("/item/create", (request, response) => {
     response.render("createItem");
-   });
+});
 
 
-   router.post("/item/create", upload.single("filetoupload"), (req, res, next) => {
+router.post("/item/create", upload.single("filetoupload"), (req, res, next) => {
 
-       console.log("Error req body",req.body)
+    console.log("Error req body", req.body)
     const file = req.file;
     if (!file) {
-      const error = new Error("Please upload a file");
-      error.httpStatusCode = 400;
-      req.flash("error",error)
-      return next(error);
+        const error = new Error("Please upload a file");
+        error.httpStatusCode = 400;
+        req.flash("error", error)
+        return next(error);
     }
 
-    let item=new Item(req.body)
+    let item = new Item(req.body)
     item.image = "/uploads/" + file.filename;
 
     item
-    .save()
-    .then(() => {
-      res.redirect("/item");
-      req.flash("success","item created")
-    })
-    .catch(err => {
-      console.log(err);
-      res.redirect("/item")
-      req.flash("error",err)
-    });
+        .save()
+        .then(() => {
+            res.redirect("/home");
+            req.flash("success", "item created")
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect("/home")
+            req.flash("error", err)
+        });
 });
 
 module.exports = router;
